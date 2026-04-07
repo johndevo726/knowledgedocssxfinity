2c9a00ff49f8d3a52a0bb6d723b1a2a30a920ec7
johndevo726 <johndevo726@protonmail.com>
2026-04-08 02:32:25 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "My Email Account Comcast Xfinity Login: How Do I Access My Xfinity Email Easily?"
+html_title = "Comcast Xfinity Log In to My Account: How Do I Access My Xfinity Email and What Can I Do With It?"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
